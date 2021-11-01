78c68d3321bcc3e21feb49e658d746174ccfd18c
Konstantin Puchkov <puchkovkostya1991@gmail.com>
2021-11-01 23:06:34 +0300

    Update data jpa configuration and test

--- src/main/java/ru/gsa/biointerface/configuration/ApplicationConfiguration.java
+++ src/main/java/ru/gsa/biointerface/configuration/ApplicationConfiguration.java
@@ -1,19 +1,23 @@
 package ru.gsa.biointerface.configuration;
 
-import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+import java.util.Objects;
+import java.util.Properties;
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
 
 /**
  * Created by Gavrilov Stepan (itgavrilov@gmail.com) on 29/10/2021
@@ -22,25 +26,42 @@ import javax.sql.DataSource;
 @EnableTransactionManagement
 @ComponentScan("ru.gsa.biointerface")
 @EnableJpaRepositories(basePackages = "ru.gsa.biointerface.repository")
+@PropertySource("classpath:hibernate.properties")
 public class ApplicationConfiguration {
+    @Autowired
+    private Environment env;
+
+    private String getProperty(String key) {
+        return Objects.requireNonNull(env.getProperty(key));
+    }
+
+    private Properties getProperties() {
+        Properties connectionProperties = new Properties();
+        connectionProperties.put("hibernate.dialect", getProperty("hibernate.dialect"));
+
+        return connectionProperties;
+    }
+
     @Bean
     public DataSource dataSource() {
-        BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("org.sqlite.JDBC");
-        dataSource.setUrl("jdbc:sqlite:./BCsqLite.s3db");
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(getProperty("hibernate.connection.driver_class"));
+        dataSource.setUrl(getProperty("hibernate.connection.url"));
+        dataSource.setConnectionProperties(getProperties());
 
         return dataSource;
     }
 
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
         HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
         vendorAdapter.setGenerateDdl(true);
 
         LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
         factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPersistenceProviderClass(HibernatePersistenceProvider.class);
         factory.setPackagesToScan("ru.gsa.biointerface.domain.entity");
-        factory.setDataSource(dataSource());
+        factory.setDataSource(dataSource);
 
         return factory;
     }
--- src/test/java/ru/gsa/biointerface/repository/database/DataSourceTest.java
+++ src/test/java/ru/gsa/biointerface/repository/database/DataSourceTest.java
@@ -1,25 +1,40 @@
 package ru.gsa.biointerface.repository.database;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.gsa.biointerface.configuration.ApplicationConfiguration;
+import ru.gsa.biointerface.domain.entity.Device;
 import ru.gsa.biointerface.domain.entity.PatientRecord;
 import ru.gsa.biointerface.service.PatientRecordService;
 
 
 class DataSourceTest {
-
     @Test
-    void getSessionFactory() {
+    public void connection() {
         try (AnnotationConfigApplicationContext context
                      = new AnnotationConfigApplicationContext(ApplicationConfiguration.class)) {
-            PatientRecordService service = context.getBean(PatientRecordService.class);
+            SessionFactory sessionFactory = context.getBean(SessionFactory.class);
 
-            try {
-                PatientRecord entity = service.getById(3L);
-                System.out.println(entity);
-            } catch (Exception e) {
-                e.printStackTrace();
+            Assertions.assertTrue(sessionFactory.isOpen());
+
+            Device deviceExpected = new Device();
+            deviceExpected.setId(1000);
+            deviceExpected.setComment("test");
+
+            try (Session session = sessionFactory.openSession()) {
+                Transaction transaction = session.beginTransaction();
+                session.saveOrUpdate(deviceExpected);
+                transaction.commit();
+            }
+
+            try (Session session = sessionFactory.openSession()) {
+                Device device1 = session.get(Device.class, 1000L);
+                Assertions.assertEquals(deviceExpected.getId(), device1.getId());
+                Assertions.assertEquals(deviceExpected.getComment(), device1.getComment());
             }
         }
     }
