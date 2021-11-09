2cb47849b58b28bd35711f0ed9f8fa797747e8b1
itgavrilov <itgavrilov@gmail.com>
2021-11-09 18:05:48 +0300

    - поигрался с jsp

--- src/main/java/ru/gsa/biointerface/controller/MainController.java
+++ src/main/java/ru/gsa/biointerface/controller/MainController.java
@@ -5,16 +5,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 /**
  * Created by Gavrilov Stepan (itgavrilov@gmail.com) on 05/11/2021
  */
 @Controller
+@RequestMapping(value = "/")
 public class MainController {
     private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
 
-    @GetMapping(value = "/")
+    @GetMapping
     public String loadHomePage(Model model) {
         System.out.println("sout - Load home page");
         LOGGER.info("LOGGER - Load home page");
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -1,10 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
+<%--
+  Created by IntelliJ IDEA.
+  User: ItGavrilov
+  Date: 09.11.2021
+  Time: 16:33
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
 <head>
     <meta charset="UTF-8">
-    <title>Halo World!</title>
+    <title>BiointerfaceController(server)</title>
+    <link rel="stylesheet" href="/css/dropdown.css"/>
 </head>
 <body>
-<h2>${message}</h2>
+    <div id="menu">
+        <%@include  file="topMenu.html" %>
+    </div>
+    <div id="content">
+        <h2>${message}</h2>
+    </div>
 </body>
 </html>
