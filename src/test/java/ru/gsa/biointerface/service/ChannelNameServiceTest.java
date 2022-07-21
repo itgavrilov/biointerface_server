e2c728f1b03de92b90ee99add37f83912b6c767b
s.gavrilov <s.gavrilov@t1-consulting.ru>
2022-07-21 14:38:29 +0300

    add unit tests for ChannelNameService

--- src/test/java/ru/gsa/biointerface/service/ChannelNameServiceTest.java
+++ src/test/java/ru/gsa/biointerface/service/ChannelNameServiceTest.java
@@ -10,10 +10,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import ru.gsa.biointerface.domain.entity.Device;
-import ru.gsa.biointerface.dto.DeviceDTO;
+import ru.gsa.biointerface.domain.entity.ChannelName;
+import ru.gsa.biointerface.dto.ChannelNameDTO;
 import ru.gsa.biointerface.exception.NotFoundException;
-import ru.gsa.biointerface.repository.DeviceRepository;
+import ru.gsa.biointerface.repository.ChannelNameRepository;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,22 +28,22 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class DeviceServiceUnitTest {
+class ChannelNameServiceTest {
 
     private final EasyRandom generator = new EasyRandom();
 
     @Mock
-    private DeviceRepository repository;
+    private ChannelNameRepository repository;
 
     @InjectMocks
-    private DeviceService service;
+    private ChannelNameService service;
 
     @Test
     void findAll() {
-        List<Device> entities = generator.objects(Device.class, 5).toList();
+        List<ChannelName> entities = generator.objects(ChannelName.class, 5).toList();
         when(repository.findAll()).thenReturn(entities);
 
-        List<Device> entityTests2 = service.findAll();
+        List<ChannelName> entityTests2 = service.findAll();
         assertNotNull(entityTests2);
         assertIterableEquals(entities, entityTests2);
         verify(repository).findAll();
@@ -53,7 +53,7 @@ class DeviceServiceUnitTest {
     void findAll_empty() {
         when(repository.findAll()).thenReturn(new ArrayList<>());
 
-        List<Device> entityTests1 = service.findAll();
+        List<ChannelName> entityTests1 = service.findAll();
         assertNotNull(entityTests1);
         assertIterableEquals(new ArrayList<>(), entityTests1);
         verify(repository).findAll();
@@ -61,21 +61,20 @@ class DeviceServiceUnitTest {
 
     @Test
     void findAllPageable() {
-        List<Device> entities = generator.objects(Device.class, 15).toList();
+        List<ChannelName> entities = generator.objects(ChannelName.class, 15).toList();
         Pageable pageable = PageRequest.of(0, 5);
 
         while (pageable.getPageNumber() * pageable.getPageSize() <= entities.size()) {
             int start = pageable.getPageNumber() * pageable.getPageSize();
             int end = Math.min(start + pageable.getPageSize(), entities.size());
-            List<Device> pageList = entities.subList(start, end);
-            Page<Device> entityPage2 = new PageImpl<>(pageList, pageable, pageList.size());
+            List<ChannelName> pageList = entities.subList(start, end);
+            Page<ChannelName> entityPage2 = new PageImpl<>(pageList, pageable, pageList.size());
             when(repository.findAll(pageable)).thenReturn(entityPage2);
 
-            Page<Device> entityPageTests2 = service.findAll(pageable);
+            Page<ChannelName> entityPageTests2 = service.findAll(pageable);
             assertNotNull(entityPageTests2);
             assertIterableEquals(entityPage2, entityPageTests2);
             verify(repository).findAll(pageable);
-
             pageable = PageRequest.of(pageable.getPageNumber() + 1, pageable.getPageSize());
         }
     }
@@ -83,10 +82,10 @@ class DeviceServiceUnitTest {
     @Test
     void findAllPageable_empty() {
         Pageable pageable = PageRequest.of(0, 5);
-        Page<Device> entityPage1 = new PageImpl<>(new ArrayList<>(), pageable, 0);
+        Page<ChannelName> entityPage1 = new PageImpl<>(new ArrayList<>(), pageable, 0);
         when(repository.findAll(pageable)).thenReturn(entityPage1);
 
-        Page<Device> entityPageTests1 = service.findAll(pageable);
+        Page<ChannelName> entityPageTests1 = service.findAll(pageable);
         assertNotNull(entityPageTests1);
         assertEquals(entityPage1, entityPageTests1);
         verify(repository).findAll(pageable);
@@ -94,10 +93,10 @@ class DeviceServiceUnitTest {
 
     @Test
     void getById() {
-        Device entity = generator.nextObject(Device.class);
+        ChannelName entity = generator.nextObject(ChannelName.class);
         when(repository.getOrThrow(entity.getId())).thenReturn(entity);
 
-        Device entityTest = service.getById(entity.getId());
+        ChannelName entityTest = service.getById(entity.getId());
         assertNotNull(entityTest);
         assertEquals(entity, entityTest);
         verify(repository).getOrThrow(entity.getId());
@@ -105,26 +104,26 @@ class DeviceServiceUnitTest {
 
     @Test
     void getById_rnd() {
-        int rndId = generator.nextInt();
-        String message = String.format("Device(id=%s) is not found", rndId);
-        when(repository.getOrThrow(rndId)).thenThrow(new NotFoundException(message));
+        int rnd = generator.nextInt();
+        String message = String.format("ChannelName(id=%s) is not found", rnd);
+        when(repository.getOrThrow(rnd)).thenThrow(new NotFoundException(message));
 
-        assertThrows(NotFoundException.class, () -> service.getById(rndId), message);
-        verify(repository).getOrThrow(rndId);
+        assertThrows(NotFoundException.class, () -> service.getById(rnd), message);
+        verify(repository).getOrThrow(rnd);
     }
 
     @Test
     void save() {
-        DeviceDTO dto = generator.nextObject(DeviceDTO.class);
-        Device entity = new Device(
+        ChannelNameDTO dto = generator.nextObject(ChannelNameDTO.class);
+        ChannelName entity = new ChannelName(
                 dto.getId(),
-                dto.getAmountChannels(),
+                dto.getName(),
                 dto.getComment(),
                 new ArrayList<>());
         when(repository.findById(entity.getId())).thenReturn(Optional.of(entity));
         when(repository.save(entity)).thenReturn(entity);
 
-        Device entityTest = service.save(dto);
+        ChannelName entityTest = service.save(dto);
         assertNotNull(entityTest);
         assertEquals(entity, entityTest);
         verify(repository).findById(entity.getId());
@@ -132,19 +131,8 @@ class DeviceServiceUnitTest {
     }
 
     @Test
-    void save_entity() {
-        Device entity = generator.nextObject(Device.class);
-        when(repository.save(entity)).thenReturn(entity);
-
-        Device entityTest = service.save(entity);
-        assertNotNull(entityTest);
-        assertEquals(entity, entityTest);
-        verify(repository).save(entity);
-    }
-
-    @Test
     void delete() {
-        Device entity = generator.nextObject(Device.class);
+        ChannelName entity = generator.nextObject(ChannelName.class);
         when(repository.getOrThrow(entity.getId())).thenReturn(entity);
 
         assertDoesNotThrow(() -> service.delete(entity.getId()));
@@ -155,7 +143,7 @@ class DeviceServiceUnitTest {
     @Test
     void delete_rnd() {
         int rnd = generator.nextInt();
-        String message = String.format("Device(id=%s) is not found", rnd);
+        String message = String.format("ChannelName(id=%s) is not found", rnd);
         when(repository.getOrThrow(rnd)).thenThrow(new NotFoundException(message));
 
         assertThrows(NotFoundException.class, () -> service.delete(rnd), message);
